a0fe0ac7aeeac1490d57b3f5971da612b6476f13
Anton Balter <antbalter@gmail.com>
2022-06-22 23:05:40 +0300

    HW#03 + Additional Tasks

--- src/telran/numbers/SportLotoAppl.java
+++ src/telran/numbers/SportLotoAppl.java
@@ -7,10 +7,10 @@ public class SportLotoAppl {
 	public static void main(String[] args) {
 		int res[] = new int[7];
 		for (int i = 0; i < res.length; i++) {
-			res[i] = getRandomNumber(1, 49);
-			while (ArrayInt.indexOf(res, res[i]) < 0) {
+			do {
 				res[i] = getRandomNumber(1, 49);
 			}
+			while (ArrayInt.indexOf(res, res[i]) >= 0); 
 		}
 		System.out.println(Arrays.toString(res));
 	}
