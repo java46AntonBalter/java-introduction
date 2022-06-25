5963ed17097a75e39df7a6af65b4a6da6795f7bb
Anton Balter <antbalter@gmail.com>
2022-06-25 19:51:54 +0300

    HW#4 solution

--- src/telran/numbers/ArrayInt.java
+++ src/telran/numbers/ArrayInt.java
@@ -3,11 +3,11 @@ package telran.numbers;
 import java.util.Arrays;
 
 public class ArrayInt {
-
 	public static int[] addNumber(int ar[], int number) {
 		int res[] = Arrays.copyOf(ar, ar.length + 1);
-		res[res.length - 1] = number;
+		res[ar.length] = number;
 		return res;
+
 	}
 
 	public static int[] insertNumber(int ar[], int index, int number) {
@@ -20,12 +20,12 @@ public class ArrayInt {
 
 	public static int[] removeNumber(int ar[], int index) {
 		int res[] = new int[ar.length - 1];
-        System.arraycopy(ar, 0, res, 0, index);
-        if (ar.length != index) {
-            System.arraycopy(ar, index + 1, res, index, ar.length - index - 1);
-        }
-        return res;
+		System.arraycopy(ar, 0, res, 0, index);
+		System.arraycopy(ar, index + 1, res, index, res.length - index);
+		return res;
+
 	}
+
 	public static int indexOf(int ar[], int number) {
 		int res = -1;
 		for (int i = 0; i < ar.length; i++) {
@@ -35,5 +35,63 @@ public class ArrayInt {
 			}
 		}
 		return res;
+	}
+
+	public static void sort(int[] ar) {
+		boolean swapped;
+		if (ar == null || ar.length == 0) {
+			return;
+		}
+		for (int i = 0; i < ar.length; i++) {
+			swapped = false;
+			swapped = moveMaxToEnd(ar, i, swapped);
+			if (swapped == false) {
+				break;
+			}
+		}
+	}
+
+	private static boolean moveMaxToEnd(int[] ar, int i, boolean swapped) {
+		for (int j = 1; j < ar.length - i; j++) {
+			if (ar[j - 1] > ar[j]) {
+				swap(ar, j);
+				swapped = true;
+			}
+		}
+		return swapped;
+	}
+
+	private static void swap(int[] ar, int index) {
+		int tmp = ar[index - 1];
+		ar[index - 1] = ar[index];
+		ar[index] = tmp;
+
+	}
+
+	public static int binaryIndexOf(int[] ar, int number) {
+		int res = 0;
+		int left = 0;
+		int right = ar.length - 1;
+		int middle = ar.length / 2;
+		while (left <= right && ar[middle] != number) {
+			if (ar[middle] < number) {
+				left = middle + 1; // looking for the number will be in right part of the array
+			} else {
+				right = middle - 1; // looking for the number will be in the left part of the array
+			}
+			middle = (left + right) / 2;
+		}
+		res = middle;
+		while (middle > 0 && ar[middle] == number && ar[middle - 1] == number) {
+			res = --middle;
+		}
+		if (ar[middle] != number) {
+			res = -(left + 1);
+		}
+
+		return res;
+		// TODO
+		// fix the code for performing the method in accordance with the above
+		// definition (see the tests)
 	}
 }
