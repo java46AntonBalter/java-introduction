a70537e9a6a8c2e5b5ac99e188b88c391138f5e2
Anton Balter <antbalter@gmail.com>
2022-06-30 18:32:39 +0300

    cw05 hw solution

--- src/telran/numbers/TictactoeGame.java
+++ src/telran/numbers/TictactoeGame.java
@@ -1,89 +1,85 @@
 package telran.numbers;
 
 public class TictactoeGame {
+/**
+ * 
+ * @param matrix square matrix (nRows == nColumns; where nRows >= 3)
+ * @param nRow (number of row)
+ * @param nCol (number of column)
+ * @param symb (being filled symbol either 'x' or '0')
+ * @return 0 - game isn't over; 1 - game is over with the winner's move; 2 - game is over with draw
+ */
 	public static int tictactoeMove(char matrix[][], int nRow, int nCol, char symb) {
-		matrix[nRow][nCol] = symb;
-		return checkStatus(matrix);
-	}
-
-	public static int checkStatus(char[][] matrix) {
-		
-		return checkifSquare(matrix) == 0 ? 
-			((checkGameOver(matrix) == 0 && checkWin(matrix) == 3) ? 0 : checkWin(matrix)) : -1 ;
-	}
-	
-	public static int checkifSquare(char[][] matrix) {
-		for (int i = 0; i < matrix.length; i++) {
-			if (matrix.length != matrix[i].length)
-				return -1;
-		}
+		matrix[nRow][ nCol] = symb;
+		if(isFullRow(matrix, nRow, symb) || isFullColumn(matrix, nCol, symb)) return 1;
+		if (isLeftDiagonal(matrix,nRow, nCol, symb) || isRightDiagonal(matrix, nRow, nCol, symb)) return 1;
+		if (isDraw(matrix)) return 2;
 		return 0;
 	}
-
-	public static int checkGameOver(char[][] matrix) {
+	private static boolean isDraw(char[][] matrix) {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix.length; j++) {
 				if (matrix[i][j] != 'x' && matrix[i][j] != '0') {
-					return 0;
+					return false;
 				}
 			}
 		}
-		return 1;
+		return true;
 	}
-
-	public static int checkWin(char[][] matrix) {
-		return (checkWinRow(matrix) == 1 || checkWinCollumn(matrix) == 1 || checkWinDiagonal1(matrix) == 1
-				|| checkWinDiagonal2(matrix) == 1) ? 1 : 3;
+	private static boolean isRightDiagonal(char[][] matrix, int row, int column, char symb) {
+		
+		if (isOnRightDiagonal(row, column, matrix.length - 1)) {
+			for (int i = 0, j = matrix.length - 1; i < matrix.length ; i++, j--) {
+				if (matrix[i][j] != symb) {
+					return false;
+				}
+			}
+			return true;
+		
+		}
+		return false;
 	}
-
-	public static int checkWinRow(char[][] matrix) {
+	private static boolean isOnRightDiagonal(int row, int column, int maxIndex) {
+		
+		return row + column == maxIndex;
+	}
+	private static boolean isLeftDiagonal(char[][] matrix, int row, int column, char symb) {
+		if (isOnLeftDiagonal(row, column)) {
+			for (int i = 0, j = 0; i < matrix.length; i++, j++) {
+				if (matrix[i][j] != symb) {
+					return false;
+				}
+			}
+			return true;
+		
+		}
+		return false;
+		
+	}
+	private static boolean isOnLeftDiagonal(int row, int column) {
+		
+		return row == column;
+	}
+	private static boolean isFullColumn(char[][] matrix, int column, char symb) {
+		boolean res = true;
 		for (int i = 0; i < matrix.length; i++) {
-			int countMatch = 0;
-			for (int j = 0; j < matrix.length; j++) {
-				if (matrix[i][j] == matrix[i][0]) {
-					++countMatch;
-				}
-			}
-			if (countMatch == matrix.length) {
-				return 1;
+			if (matrix[i][column] != symb) {
+				res= false;
+				break;
 			}
 		}
-		return 3;
+		return res;
 	}
-
-	public static int checkWinCollumn(char[][] matrix) {
-		for (int i = 0; i < matrix.length; i++) {
-			int countMatch = 0;
-			for (int j = 0; j < matrix.length; j++) {
-				if (matrix[j][i] == matrix[0][i]) {
-					++countMatch;
-				}
-			}
-			if (countMatch == matrix.length) {
-				return 1;
+	private static boolean isFullRow(char[][] matrix, int row, char symb) {
+		boolean res = true;
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[row][j] != symb) {
+				res = false;
+				break;
 			}
 		}
-		return 3;
+		return res;
 	}
 
-	public static int checkWinDiagonal1(char[][] matrix) {
-		int countMatch = 0;
-		for (int i = 0, j = 0; i < matrix.length - 1; i++, j++) {
-
-			if (matrix[i][j] == matrix[i + 1][j + 1]) {
-				++countMatch;
-			}
-		}
-		return (countMatch == matrix[0].length - 1) ? 1 : 3;
-	}
-
-	public static int checkWinDiagonal2(char[][] matrix) {
-		int countMatch = 0;
-		for (int i = matrix.length - 1, j = 0; j < matrix.length - 1; i--, j++) {
-			if (matrix[i][j] == matrix[i - 1][j + 1]) {
-				++countMatch;
-			}
-		}
-		return (countMatch == matrix.length - 1) ? 1 : 3;
-	}
 }
+
