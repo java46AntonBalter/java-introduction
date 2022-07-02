5a3d543cf1a4efce950d5a9ed4f7794de637efb1
Anton Balter <antbalter@gmail.com>
2022-07-02 22:56:26 +0300

    HW06 and AD03 solution

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -4,10 +4,10 @@ public class Strings {
 
 	public static int[] deepNoRepeatedCompare(String str1, String str2) {
 		int[] res = new int [2];
-		for(int i = 0; i < str2.toCharArray().length; i++) {
-			if(str1.indexOf(str2.toCharArray()[i]) == i) {
+		for(int i = 0; i < str2.length(); i++) {
+			if(str1.indexOf(str2.charAt(i)) == i) {
 				++res[0];
-			} else if (str1.indexOf(str2.toCharArray()[i]) >= 0) {
+			} else if (str1.indexOf(str2.charAt(i)) >= 0) {
 				++res[1];
 			}		
 		}
@@ -15,7 +15,7 @@ public class Strings {
 }
 
 	public static boolean isAnagram(String str1, String str2) {
-		if (str1.toCharArray().length != str2.toCharArray().length) return false;
+		if (str1.length() != str2.length()) return false;
 		int count[] = new int[26];
 		for (int i = 0; i < str1.length(); i++) {
 	        count[str1.toLowerCase().charAt(i) - 97]++;
