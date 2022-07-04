941aa13353adb115185263ab429e0146d7e81f1e
Anton Balter <antbalter@gmail.com>
2022-07-05 00:34:30 +0300

    HW07 solution

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -1,31 +1,65 @@
 package telran.text;
 
 public class Strings {
+	/**
+	 * 
+	 * @param name1 - first name
+	 * @param name2 - second name
+	 * @return either "match" or "no match" in accordance to the comments (see TODO)
+	 */
+	static public String matches(String name1, String name2) {
+		String[] name1SplitArray = name1.toLowerCase().split(" ");
+		String[] name2SplitArray = name2.toLowerCase().split(" ");
+		int length1 = name1SplitArray.length;
+		int length2 = name2SplitArray.length;
+		int length = length1 > length2 ? length1 : length2;
+		
 
-	public static int[] deepNoRepeatedCompare(String str1, String str2) {
-		int[] res = new int [2];
-		for(int i = 0; i < str2.length(); i++) {
-			if(str1.indexOf(str2.charAt(i)) == i) {
-				++res[0];
-			} else if (str1.indexOf(str2.charAt(i)) >= 0) {
-				++res[1];
-			}		
+		if(length1 == 1 && length2 == 1 && name1SplitArray[0].compareTo(name2SplitArray[0]) == 0) return "match";
+		
+		for(int i = 0; i < length; i++) {
+			
 		}
-		return res;	
-}
+	
+//	 TODO
+//      String matches(String name1, String name2)
+//      Names match under the following conditions, after breaking each one into "name parts" on whitespace :
+//      - Two name parts match if they are the same (case insensitive) or one is a single letter initial and the other is longer but starts with the same letter.
+//      - A name part in  the name1 may be missing from the name2
+//      - Name parts in one name must not contradict name parts in the other
+//      - Name parts that match must be in the same order in both names
+	return"no match";
 
-	public static boolean isAnagram(String str1, String str2) {
-		if (str1.length() != str2.length()) return false;
-		int count[] = new int[26];
-		for (int i = 0; i < str1.length(); i++) {
-	        count[str1.toLowerCase().charAt(i) - 97]++;
-	        count[str2.toLowerCase().charAt(i) - 97]--;
-	    }
-	    for (int i = 0; i < count.length; i++) {
-	        if (count[i] != 0) {
-	            return false;
-	        }
-	    }
-	    return true;
+	}
+	/**
+	 * sorts array of strings
+	 * @param strNumbers array of strings containing the positive integer numbers
+	 * length of each string can not be more than three symbols
+	 * String containing "123" should be greater than string containing "23" as the number 123 greater than
+	 * number 23
+	 */
+	static public String[] sortStringsAsNumbers(String[] strNumbers) {
+		int[] helper = fillInHelper(strNumbers);
+		return fillInSortedNumbersAsStrings(helper, strNumbers);
+	}
+
+	static private int[] fillInHelper(String[] strNumbers) {
+		int[] helper = new int[1000];
+		for (int i = 0; i < strNumbers.length; i++) {
+			helper[Integer.parseInt(strNumbers[i])]++;
+		}
+		return helper;
+	}
+
+	static private String[] fillInSortedNumbersAsStrings(int[] helper, String[] strNumbers) {
+		int index = 0;
+		for (int i = 0; i < helper.length; i++) {
+			while (helper[i] > 0) {
+				strNumbers[index] = String.valueOf(i);
+				index++;
+				helper[i]--;
+			}
+		}
+		return strNumbers;
 	}
 }
--- src/telran/text/tests/StringsClassTests.java
+++ src/telran/text/tests/StringsClassTests.java
@@ -8,37 +8,29 @@ import telran.text.Strings;
 
 class StringsClassTests {
 
+//	@Test
+//	void matchesTest() {
+//		String str1 = "David";
+//		String str2 = "david";
+//		assertEquals("match", Strings.matches(str1, str2));
+//		//TODO
+//		//        Examples:
+////	        - David vs. david: match
+////	        - John F vs. John Fitzgerald: match
+////	        - John K vs. John Fitzgerald: no match
+////	        - Anna Maria Magdalena vs. Anna Magdalena: match
+////	        - Anna Maria Magdalena vs. Maria Magdalena: match
+////	        - Anna Maria Magdalena vs. Anna Maria Roberta: no match
+////	        - Anna Maria Magdalena vs. Anna Magdalena Roberta: no match
+////	        - Anna Maria Magdalena vs. Anna Magdalena Maria: no match
+//	}
 	@Test
-	void deepCompareTest() {
-		String str1 = "123,tT89";
-		String str2 = "123,tT89";
-		String str3 = "19";
-		String str5 = "98Tt,321";
-		String str4 ="kugy";
-		int expected1[] = {8, 0};
-		int expected2[] = {1, 1};
-		int expected3[] = {0, 8};
-		int expected4[] = {0, 0};
-		assertArrayEquals(expected1, Strings.deepNoRepeatedCompare(str1, str2));
-		assertArrayEquals(expected2, Strings.deepNoRepeatedCompare(str1, str3));
-		assertArrayEquals(expected3, Strings.deepNoRepeatedCompare(str1, str5));
-		assertArrayEquals(expected4, Strings.deepNoRepeatedCompare(str1, str4));
-	}
-
-	
-	@Test
-	void isAnagramTests() {
-		String str1 = "Hello";
-		String str2 = "Helll";
-		String str3 = "eloHl";
-		String str4 = "Helo";
-		String str5 = "astronomer";
-		String str6 = "moonstarer";
-		assertTrue(Strings.isAnagram(str1, str3));
-		assertFalse(Strings.isAnagram(str1, str2));
-		assertFalse(Strings.isAnagram(str1, str4));
-		assertTrue(Strings.isAnagram(str1, str1));
-		assertTrue(Strings.isAnagram(str5, str6));
+	void sortStringsAsNumbersTest() {
+		String[] str1 = { "125", "7", "7", "7", "650", "321", "321", "1", "1", "0", "0", "999", "999", "977", "56",
+				"56" };
+		String[] expected = {"0", "0", "1", "1", "7", "7", "7", "56", "56", "125", "321", "321", "650", "977", "999", "999"};
+		Strings.sortStringsAsNumbers(str1);
+		assertArrayEquals(expected, str1);
 	}
 
 }
